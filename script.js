6256335673c32e9f130d881132c2a6106263fd8f
Mandrade6892 <2002andradejr@gmail.com>
2026-04-16 17:48:51 -0700

    Fix HTML structure and enhance pet interaction logic in JavaScript

--- script.js
+++ script.js
@@ -20,4 +20,42 @@ function clickedTreatButton(){
   pet_info.happiness += 2;
   pet_info.weight += 1;
 
+    $('.pet-message').text("Buddy says: Yum!");
+    $('.pet-message').toggle(); // jQuery Method #1
+    $('.pet-message').toggle();
+
+    checkAndUpdatePetInfoInHtml();
 }
+
+function clickedPlayButton() {
+  pet_info.happiness += 3;
+  pet_info.weight -= 1;
+  energy -= 2;
+
+    $('.pet-message').text("Buddy says: That was fun!");
+    $('.pet-message').toggle();
+    $('.pet-message').toggle();
+
+    checkAndUpdatePetInfoInHtml();
+}
+
+function checkAndUpdatePetInfoInHtml(){
+  checkWeightAndHappinessBeforeUpdating();
+  checkAndUpdatePetInfoInHtml();
+}
+
+function checkWeightAndHappinessBeforeUpdating() {
+  if (pet_info.weight < 0) {
+    pet_info.weight = 0;
+  }
+
+  if (pet_info.happiness < 0) {
+    pet_info.happiness = 0;
+  }
+
+  if (energy < 0) {
+    energy = 0;
+  }
+
+}
+
